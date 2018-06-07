16f1e4ea6d81b2fa0516305ce050943c229cd959
Maginet66 <38592268+Maginet66@users.noreply.github.com>
2018-06-07 07:11:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.sendMessage('pong');
+    	message.channel.sendMessage('pong');
   	}
 });
 
