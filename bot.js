de8260fafc945398cccb52b5f0b4468aee7b330a
Maginet66 <38592268+Maginet66@users.noreply.github.com>
2018-06-07 07:30:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,14 +7,14 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'gizlikomudxd') {
-        message.channel.sendMessage('`');
-    	message.channel.sendMessage('__**Kurallar**__');
-        message.channel.sendMessage('1- Küfür, hakaret, argo gibi kelimeler dozunda kullanılmalıdır. (Eğer bir şahısa hakaret edip ve kendiside bu durumdan rahatsız oluyorsa ceza alabilirsiniz.)');
-        message.channel.sendMessage('2- Her kişiye saygı gösterin saygısızlık yapmayınız.');
-        message.channel.sendMessage('3- Spam yapmayınız.');
-        message.channel.sendMessage('4- Kimsenin başka bir kimseden üstünlük gösterilmemesi gerekir.');
-        message.channel.sendMessage('5- Başkalarını rahatsız etmeyiniz.');
-        message.channel.sendMessage('6- Odalarda bağırmayınız.');
+        message.channel.sendMessage('');
+    	message.channel.sendMessage('`__**Kurallar**__`');
+        message.channel.sendMessage('`1- Küfür, hakaret, argo gibi kelimeler dozunda kullanılmalıdır. (Eğer bir şahısa hakaret edip ve kendiside bu durumdan rahatsız oluyorsa ceza alabilirsiniz.)`');
+        message.channel.sendMessage('`2- Her kişiye saygı gösterin saygısızlık yapmayınız.`');
+        message.channel.sendMessage('`3- Spam yapmayınız.`');
+        message.channel.sendMessage('`4- Kimsenin başka bir kimseden üstünlük gösterilmemesi gerekir.`');
+        message.channel.sendMessage('`5- Başkalarını rahatsız etmeyiniz.`');
+        message.channel.sendMessage('`6- Odalarda bağırmayınız.`');
         message.channel.sendMessage('`');
   	}
 });
