b94583818f1dd7029ba90598f5f9866c7bfdbb00
Maginet66 <38592268+Maginet66@users.noreply.github.com>
2018-06-07 07:05:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (msg.content === 'asdkgfasfkasfewtgesjgmses') {
-    	message.send('asdasd');
+    	message.reply('asdasd');
   	}
 });
 
