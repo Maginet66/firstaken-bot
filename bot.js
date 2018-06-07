3c3c2de04edc382f3920b3af353d30b81d396911
Maginet66 <38592268+Maginet66@users.noreply.github.com>
2018-06-07 06:50:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,13 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'asdkgfasfkasfewtgesjgmses') {
-    	message.reply('__**Kurallar**__
-**1-** Küfür, hakaret, argo gibi kelimeler dozunda kullanılmalıdır. (Eğer bir şahısa hakaret edip ve kendiside bu durumdan rahatsız oluyorsa ceza alabilirsiniz.)
-**2-** Her kişiye saygı gösterin saygısızlık yapmayınız.
-**3-** Spam yapmayınız.
-**4-** Kimsenin başka bir kimseden üstünlük gösterilmemesi gerekir.
-**5-** Başkalarını rahatsız etmeyiniz.
-**6-** Odalarda bağırmayınız.');
+    	message.reply('asdasd');
   	}
 });
 
