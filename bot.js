98c6e3753a357aef199caa235f48b441310020ec
Maginet66 <38592268+Maginet66@users.noreply.github.com>
2018-06-07 07:18:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,12 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.channel.sendMessage('Kurallar');
-        message.channel.sendMessage('Kasdsadsadr');
+        message.channel.sendMessage('1- Küfür, hakaret, argo gibi kelimeler dozunda kullanılmalıdır. (Eğer bir şahısa hakaret edip ve kendiside bu durumdan rahatsız oluyorsa ceza alabilirsiniz.)');
+        message.channel.sendMessage('2- Her kişiye saygı gösterin saygısızlık yapmayınız.');
+        message.channel.sendMessage('3- Spam yapmayınız.');
+        message.channel.sendMessage('4- Kimsenin başka bir kimseden üstünlük gösterilmemesi gerekir.');
+        message.channel.sendMessage('5- Başkalarını rahatsız etmeyiniz.');
+        message.channel.sendMessage('6- Odalarda bağırmayınız.');
   	}
 });
 
